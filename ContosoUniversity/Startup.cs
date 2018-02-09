fae1f91aa25a1428b22050163e69df546de15074
LeeTrent <Lee.Trent.1@gmail.com>
2018-02-08 20:49:50 -0600

    Initial commit

---
+++ ContosoUniversity/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Database Connection Parameters
+            String connectionString = Environment.GetEnvironmentVariable("LOCAL_CONNECTION_STRING");
+
+            // WRITE CONNECTION STRING TO THE LOG
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("[Startup] Connection String: " + connectionString);
+            Console.WriteLine("********************************************************************************");
+
+            // NOW THAT WE HAVE OUR CONNECTION STRING,
+            // WE CAN ESTABLISH OUR DB CONTEXT
+            services.AddDbContext<SchoolContext>
+            (
+		        opts => opts.UseMySQL(connectionString)
+		    );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
