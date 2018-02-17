9a921740d54cef2d56fb5f58400c770c1637e590
LeeTrent <Lee.Trent.1@gmail.com>
2018-02-17 10:22:17 -0600

    Adds validation rules to 'Students' model class

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,7 +9,8 @@ namespace ContosoUniversity.Models
         public int ID { get; set; }
         
         [Required]
-        [StringLength(50)]
+        [StringLength(50, MinimumLength=1)]
+        [Display(Name = "Last Name")]
         // Requires the first character to be upper case
         // and the remaining characters to be alphabetical
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
@@ -17,6 +18,7 @@ namespace ContosoUniversity.Models
         
         [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
         // Requires the first character to be upper case
         // and the remaining characters to be alphabetical
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
@@ -25,7 +27,17 @@ namespace ContosoUniversity.Models
         [Required]
 [       DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
